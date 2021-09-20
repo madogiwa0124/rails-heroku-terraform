0c8f87ca2674825e3b94cc0bc3cc189507c5577a
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-09-20 10:14:22 +0900

    build application on heroku by terraform
    configured
    
    * redis
    * posgresql
    * buildpacks

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "heroku" {}
+
+variable "app_name" {
+  description = "The name of the application."
+  default = "sample-app"
+}
+
+resource "heroku_app" "app" {
+  name = "${var.app_name}"
+  region = "us"
+  # (Optional) not necessary if you are only using assets precompile.used only assets:precompile
+  buildpacks = ["heroku/nodejs", "heroku/ruby"]
+  # (Optional) enviroment variables.
+  # config_vars = {
+  #   FOOBAR = "baz"
+  # }
+  # sensitive_config_vars = {
+  #   SENSITIVE_FOO_BAR = "baz"
+  # }
+}
+
+resource "heroku_addon" "database" {
+  app = "${heroku_app.app.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "redis" {
+  app = "${heroku_app.app.name}"
+  plan = "heroku-redis:hobby-dev"
+}
+
+# =========================
+# heroku formation
+# required Procfile in repository.
+# ex)
+# ```
+# web: bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
+# worker: bundle exec sidekiq
+# ```
+# =========================
+#
+# resource "heroku_formation" "web" {
+#   app = heroku_app.app.name
+#   type = "web"
+#   size = "hobby"
+#   quantity = 1
+# }
+#
+# resource "heroku_formation" "worker" {
+#   app = heroku_app.app.name
+#   type = "worker"
+#   size = "hobby"
+#   quantity = 1
+# }
